b7d6b60f13cb177f65e97bc71b792d7ac87ad422
eddie <eddie@eddies-MBP.attlocal.net>
2019-12-03 10:08:19 -0500

    first commit

---
+++ src/main/java/com/swift/f5resource/F5resourceApplication.java
@@ -0,0 +1,13 @@
+package com.swift.f5resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F5resourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F5resourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swift/f5resource/F5resourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swift.f5resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F5resourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
